500d361bf531a7928382ea5bfdb3ab08e07ff985
+ <lucy.njoroge1@student.moringaschool.com>
2023-08-23 02:15:00 +0300

    debugging test

--- lib/testing/ipdb_debugging_test.py
+++ lib/testing/ipdb_debugging_test.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-from ipdb_debugging import plus_two
+import ipdb
+from lib.ipdb_debugging import plus_two
 
-class TestIpdbDebugging:
-    '''ipdb_debugging.py'''
-    
-    def test_adds_two(self):
-        '''adds_two() adds 2 to input arg and returns sum.'''
-        assert(plus_two(3) == 5)
+def test_adds_two():
+    '''adds_two() adds 2 to input arg and returns sum.'''
+    ipdb.set_trace() 
+    assert plus_two(3) == 5
